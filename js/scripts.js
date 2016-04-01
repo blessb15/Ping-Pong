925c95b462de1dc125bde9544a01bbcce2f86707
blessb15 <blakebless7@gmail.com>
2016-04-01 15:53:01 -0700

    now takes input and checks all the rules and shoots out ping, pong, pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
 //business
 var incNum = 0;
 var runIt = function(input){
-
     while (input > incNum){
       incNum += 1;
-      var show = $(".display").append("<li>" + incNum + "</li>");
-      if (incNum % 5 === 0){
+      if (incNum % 3 === 0 && incNum % 5 === 0){
+        $(".list").append("<li>" + "pingpong" + "</li>");
+        } else if (incNum % 3 === 0){
+          $(".list").append("<li>" + "ping" + "</li>");
+          } else if (incNum % 5 === 0){
+            $(".list").append("<li>" + "pong" + "</li>");
+            } else
+              $(".list").append("<li>" + incNum + "</li>");
       }
+
     }
-}
-
-
-
-// $(".display").append("<li>" + incNum + "</li>");
-
-
-
 
 //user
 
@@ -24,9 +22,7 @@ $(document).ready(function(){
   $("#Ping-Pong").submit(function(event){
     event.preventDefault();
 
-    var show
-
-    var userInput = $("input#number").val();
+    var userInput = $("input#number").val().split(" ");
 
     runIt(userInput);
   });
