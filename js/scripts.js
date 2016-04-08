aa68f737828ad9616b5bde69e80a234076f9f6d1
blessb15 <blakebless7@gmail.com>
2016-04-08 14:30:25 -0700

    fixed business and user logic mixup.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,33 @@
 //business logic
-var incNum = 0;
   var runIt = function(input){
+    var incNum = 0;
+    var newArray = [];
     while (input > incNum){
       incNum += 1;
       if (incNum % 3 === 0 && incNum % 5 === 0){
-        $(".list").append("<li>" + "pingpong" + "</li>");
-        } else if (incNum % 3 === 0){
-          $(".list").append("<li>" + "ping" + "</li>");
-          } else if (incNum % 5 === 0){
-            $(".list").append("<li>" + "pong" + "</li>");
-            } else
-              $(".list").append("<li>" + incNum + "</li>");
+      newArray.push("pingpong");
+      } else if (incNum % 3 === 0){
+      newArray.push("ping");
+      } else if (incNum % 5 === 0){
+      newArray.push("pong");
+      } else
+      newArray.push(incNum);
     }
+    return newArray;
   }
+
+
 //user logic
 $(document).ready(function(){
   $("#Ping-Pong").submit(function(event){
     event.preventDefault();
+    $("#result").empty();
     var userInput = $("input#number").val();
-    runIt(userInput);
+    console.log(userInput);
+    var result = runIt(userInput);
+    console.log(result)
+    for(i=0; i < result.length; i++){
+    $("#result").append("<li>" + result[i] + "</li>");
+    }
   });
 });
